626bbbfbe8fb2ad96c2bb64efafde460c333cab8
Dinesh Kodali <64801249+dineshkodali@users.noreply.github.com>
2026-01-06 11:08:39 +0000

    tasks

--- components/TeamManagement.tsx
+++ components/TeamManagement.tsx
@@ -98,7 +98,7 @@ const TeamManagement: React.FC<TeamManagementProps> = ({ teams, employees, onAdd
                     </div>
                 </div>
 
-                <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                     {filteredTeams.map(team => {
                         const lead = employees.find(e => e.id === team.leaderid);
                         return (
